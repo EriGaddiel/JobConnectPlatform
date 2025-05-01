2c9d789bb39ce3961adbd3aea52f7d7f30a467eb
Darael Eri Gaddiel <daraeleri@gmail.com>
2025-05-01 15:36:43 +0100

    pagination filter

--- backend/Controllers/job.controller.js
+++ backend/Controllers/job.controller.js
@@ -49,8 +49,17 @@ export const getAllJobs = async (req, res) => {
             queryResult = queryResult.sort('-title')
         }
 
+        const page = Number(req.query.page) || 1
+        const limit = Number(req.query.limit) || 10
+        const skip = (page - 1) * limit
+
+        queryResult = queryResult.skip(skip).limit(limit)
+
+        const totalJobs = await Job.countDocuments(queryResult)
+        const numOfPage = Math.ceil(totalJobs / limit)
+
         const jobs = await queryResult
-        res.status(200).json({totalJobs: jobs.length, jobs})
+        res.status(200).json({totalJobs: totalJobs, jobs, numOfPage})
     } catch (error) {  
         res.status(500).json({ error: "Internal server error" });  
         console.log(`Error occurred in the Job controller: ${error.message}`);  
